1ab3a1e2d784c9ef147eb430047f1dc834804669
anastashka777 <147683043+anastashka777@users.noreply.github.com>
2024-08-22 12:26:43 -0400

    added javascript

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function(){
+    $("logo").click(function(){
+      $("#logo").fadeIn();
+      /*$("#div2").fadeIn("slow");
+      $("#div3").fadeIn(3000);
+    });
+  });*/
+  window.onload = function() {
+    const logo = document.getElementById('logo');
+    logo.style.opacity = 1; 
+};
